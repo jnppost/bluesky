2207af0745c071a91a58a232a027d219927fa647
ilabs-kdc <93335573+ilabs-kdc@users.noreply.github.com>
2023-02-08 13:25:12 +0100

    Dotted dashed lines implementation test in up-to-date main bluesky

---
+++ plugins/tids.py
@@ -0,0 +1,18 @@
+""" BlueSky plugin template. The text you put here will be visible
+    in BlueSky as the description of your plugin. """
+# Import the global bluesky objects. Uncomment the ones you need
+import bluesky as bs
+from bluesky import core, stack, traf, settings, navdb, sim, scr, tools, plugins
+
+
+def init_plugin():
+
+    config = {
+        # The name of your plugin
+        'plugin_name':     'TID',
+        # The type of this plugin.
+        'plugin_type':     'sim'
+        }
+
+    return config
+
